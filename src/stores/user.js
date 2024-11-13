29cf5620587ef3ea3faa43703aabe7c56e2fbc57
김본 <qhsl0615@naver.com>
2024-11-14 00:04:13 +0900

    Fix: API 응답 데이터 구조 변경(CustomResponse)에 따른 user.js 코드 수정

--- src/stores/user.js
+++ src/stores/user.js
@@ -29,7 +29,7 @@ export const useUserStore = defineStore('user', {
         const response = await api.get('users/check-username', {
           params: { userName: username}
         })
-        return response.data.isAvailable // true : 사용 가능, false : 중복
+        return response.data.data.isAvailable // true : 사용 가능, false : 중복
       } catch (error) {
         console.error('Username check error:', error)
         return false
@@ -42,7 +42,7 @@ export const useUserStore = defineStore('user', {
         const response = await api.get('users/check-email', {
           params: { userEmail }
         })
-        return response.data.isAvailable
+        return response.data.data.isAvailable
       } catch (error) {
         console.error('Email check error:', error)
         return false
@@ -84,8 +84,8 @@ export const useUserStore = defineStore('user', {
       try {
         const response = await api.get('/users/userinfo')
 
-        if (response.data) {
-          this.userInfo = response.data
+        if (response.data.data) {
+          this.userInfo = response.data.data
           return true
         }
         this.userInfo = null
@@ -110,7 +110,7 @@ export const useUserStore = defineStore('user', {
     async loadSessionFromCookies() {
       try {
         const response = await api.get('/users/check')
-        if (response.data.isAuthenticated) {
+        if (response.data.data.isAuthenticated) {
           await this.getUserInfo()
           return true
         }
