4fa1c934320af77e715df05c09a765678559569b
임혁 <cs.javah@kakao.com>
2024-11-15 18:52:45 +0900

    KakaoMap 10%

--- src/components/content/MapContent.vue
+++ src/components/content/MapContent.vue
@@ -1,40 +1,15 @@
 <script setup>
-import { Coffee, Landmark, Map, Utensils } from 'lucide-vue-next'
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card/index.js'
-import { Button } from '@/components/ui/button/index.js'
+import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
+const coordinate = {
+  lat: 37.566826,
+  lng: 126.9786567
+};
 </script>
 <template>
-  <Card>
-    <CardHeader>
-      <CardTitle>지도</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <div class="flex space-x-2 mb-4">
-        <Button variant="outline" size="sm" class="rounded-full">
-          <Coffee class="h-4 w-4 mr-2" />
-          카페
-        </Button>
-        <Button variant="outline" size="sm" class="rounded-full">
-          <Utensils class="h-4 w-4 mr-2" />
-          음식점
-        </Button>
-        <Button variant="outline" size="sm" class="rounded-full">
-          <Landmark class="h-4 w-4 mr-2" />
-          관광지
-        </Button>
-      </div>
-      <div
-        class="aspect-video bg-gray-200 rounded-md flex items-center justify-center"
-      >
-        <Map class="h-16 w-16 text-gray-400" />
-      </div>
-    </CardContent>
-  </Card>
+
+  <KakaoMap width="90vw" height="" :lat="coordinate.lat" :lng="coordinate.lng" :draggable="true">
+    <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
+  </KakaoMap>
 </template>
 
 <style scoped>
